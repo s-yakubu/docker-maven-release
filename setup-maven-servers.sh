dfafb880b08209e7513eb38c9300940a9864d1a7
s-yakubu <salifu.yakubu@genkey.com>
2024-08-08 20:22:01 +0000

    added args for maven mirrors

--- setup-maven-servers.sh
+++ setup-maven-servers.sh
@@ -1,14 +1,10 @@
+# Initialize the XML strings
 MAVEN_SERVERS_XML='';
-echo "Setup the maven server credentials for the settings.xml"
+MAVEN_MIRRORS_XML='';
 
-#For retro-compatilibity
-if [[ -n "$MAVEN_REPO_SERVER_ID" ]]; then
-  echo "WARNING: MAVEN_REPO_SERVER_ID is now deprecated, please use MAVEN_SERVERS instead."
-  export MAVEN_REPO_SERVER_PRIVATE_KEY="${SSH_ROOT_FOLDER}/id_rsa"
-  export MAVEN_REPO_SERVER_PASSPHRASE=${SSH_PASSPHRASE}
-  MAVEN_SERVERS_XML=$(envsubst < $SETTINGS_SERVER_TEMPLATE_FILE)
-fi
+echo "Setup the Maven server credentials and mirrors for the settings.xml"
 
+# Process servers
 for row in $(echo "${MAVEN_SERVERS}" | jq -r '.[] | @base64'); do
     _jq() {
         echo "${row}" | base64 -d | jq -r "${1}"
@@ -16,25 +12,31 @@ for row in $(echo "${MAVEN_SERVERS}" | jq -r '.[] | @base64'); do
    export MAVEN_REPO_SERVER_ID=$(_jq '.id')
    export MAVEN_REPO_SERVER_USERNAME=$(_jq '.username')
    export MAVEN_REPO_SERVER_PASSWORD=$(_jq '.password')
-   export MAVEN_REPO_SERVER_PRIVATE_KEY=$(_jq '.privateKey')
 
-   echo "Setup the server $MAVEN_REPO_SERVER_ID"
-   if [[ "$MAVEN_REPO_SERVER_PRIVATE_KEY" == "null" ]]; then
-        export MAVEN_REPO_SERVER_PRIVATE_KEY="${SSH_ROOT_FOLDER}/id_rsa"
-   fi
-   export MAVEN_REPO_SERVER_PASSPHRASE=$(_jq '.passphrase')
-   if [[ "$MAVEN_REPO_SERVER_PASSPHRASE" == "null" ]]; then
-        export MAVEN_REPO_SERVER_PASSPHRASE=${SSH_PASSPHRASE}
-   fi
    templateReplaced=$(envsubst < $SETTINGS_SERVER_TEMPLATE_FILE);
    MAVEN_SERVERS_XML="${MAVEN_SERVERS_XML}${templateReplaced}"
-
 done
 
+# Process mirrors
+for row in $(echo "${MAVEN_MIRRORS}" | jq -r '.[] | @base64'); do
+    _jq() {
+        echo "${row}" | base64 -d | jq -r "${1}"
+    }
+   export MAVEN_MIRROR_ID=$(_jq '.id')
+   export MAVEN_MIRROR_URL=$(_jq '.url')
+   export MAVEN_MIRROR_MIRROROF=$(_jq '.mirrorOf')
+
+   templateReplaced=$(envsubst < $SETTINGS_MIRROR_TEMPLATE_FILE);
+   MAVEN_MIRRORS_XML="${MAVEN_MIRRORS_XML}${templateReplaced}"
+done
+
+# Export the accumulated XML sections
 export MAVEN_SERVERS_XML="$MAVEN_SERVERS_XML"
+export MAVEN_MIRRORS_XML="$MAVEN_MIRRORS_XML"
 
-echo "The following servers section that is going to be replace in the settings.xml:\n $MAVEN_SERVERS_XML"
+echo "The following servers section that is going to be replaced in the settings.xml:\n $MAVEN_SERVERS_XML"
+echo "The following mirrors section that is going to be replaced in the settings.xml:\n $MAVEN_MIRRORS_XML"
 
-echo "Replacing maven servers in template ${SETTINGS_SERVER_TEMPLATE_FILE} to ${SETTINGS_TEMPLATE_FILE}"
+# Replace placeholders in the main template
 envsubst < ${SETTINGS_TEMPLATE_FILE} > ${SETTINGS_FILE}
 cat ${SETTINGS_FILE}
